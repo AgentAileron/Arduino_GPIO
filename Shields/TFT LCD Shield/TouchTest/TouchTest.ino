2ac813462056ab5dd2d1e5d5bf42bff0b40c9fa7
Rithesh Jayaram <rrjayaram@outlook.com.au>
2017-07-11 22:38:12 +1000

    Ocassionally successful sketch created - fixing now

--- Shields/TFT LCD Shield/TouchTest/TouchTest.ino
+++ Shields/TFT LCD Shield/TouchTest/TouchTest.ino
@@ -1,7 +1,12 @@
 /*  TFT Touchscreen test
  *  C: 11/07/2017 | LM: 11/07/2017
  *  ----------------------------------
- *  <Description>
+ *  This sketch doubles as a documentation database for shield experiments
+ *  
+ *  > Diplay size in pixels: __X240
+ *  > Screen coordinates aligned from top-right edge
+ *  > Analog pins for display / touchscreen can be shared
+ *  > Draw commands are static until screen reset or screen space overwritten with new command (allows pin sharing)
  *  ----------------------------------
  *  By Rithesh R Jayaram
  *  ----------------------------------
@@ -10,7 +15,7 @@
 */
 
 // LIBRARIES //
-include <Adafruit_GFX.h>      // Core graphics library
+#include <Adafruit_GFX.h>     // Core graphics library
 #include <Adafruit_TFTLCD.h>  // Hardware-specific library
 #include <TouchScreen.h>      // Touchscreen library
 
@@ -24,10 +29,42 @@ Adafruit_TFTLCD tft(A3, A2, A1, A0, A4);    // Initialise LCD Display (default w
 
 
 void setup(){
-  
+  Serial.begin(9600); // Debugging serial
+  Serial.println(F("Debug sketch for TFT LCD 2.8\""));
+  tft.reset();  // Resets tft device (instant white-screen is visual effect seen)
+
+  checkDrivers(); // Prints found drivers to serial
 }
 
 void loop(){
-  
+  TSPoint p = ts.getPoint();  // Places touchscreen input values into tri-component var 'p'
+
+  // shared touchscreen pins need to be made outputs (were being used as inputs for tft lcd)
+  pinMode(A3, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(8, OUTPUT);
+}
+
+
+// Function to print device driver found to serial
+void checkDrivers(void){
+  uint16_t identifier = tft.readID();
+
+  if(identifier == 0x9325) {
+    Serial.println(F("Found ILI9325 LCD driver"));
+  } else if(identifier == 0x9328) {
+    Serial.println(F("Found ILI9328 LCD driver"));
+  } else if(identifier == 0x7575) {
+    Serial.println(F("Found HX8347G LCD driver"));
+  } else if(identifier == 0x9341) {
+    Serial.println(F("Found ILI9341 LCD driver"));
+  } else if(identifier == 0x8357) {
+    Serial.println(F("Found HX8357D LCD driver"));
+  } else {
+    Serial.print(F("Unknown LCD driver chip: "));
+    Serial.println(identifier, HEX);
+    return;
+  }
 }
 
