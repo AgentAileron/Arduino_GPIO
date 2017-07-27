3065a976f5c76ce33a6cce24a87751f00094873a
Rithesh Jayaram <rrjayaram@outlook.com.au>
2017-07-27 23:04:22 +1000

    Added Blutooth AT mode manipulation script

---
+++ HC-05_AT-Mode-Instigator/HC-05_AT-Mode-Instigator.ino
@@ -0,0 +1,19 @@
+/*  HC-05 Bluetooth Module AT Mode interrogator
+ *  C: 27/07/2017 | LM: 27/07/2017
+ *  ----------------------------------
+ *  Activates and interacts with AT mode on BT module
+ *  (See comments for list of available options and commands
+ *  ----------------------------------
+ *  By Rithesh R Jayaram
+ *  ----------------------------------
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
